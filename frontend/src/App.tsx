05ecff26c3883048c405c76c6ac5f8ea5bdc51f3
soufienaessalah <soufien.ahmedessalah@esprit.tn>
2025-02-16 23:34:24 +0000

    feat: add sign-up page template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
-
-
+import SignUP from "./pages/signup";
+import GetInvolved from "./pages/GetInvolved";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-
+      <Route path="/get-involved" element={<GetInvolved />} />
+      <Route path="/signup" element={<SignUP />} />
     </Routes>
   );
 }
