60bbd2763dee4b84179a698f8606f52ef6a733ff
soufienaessalah <soufien.ahmedessalah@esprit.tn>
2025-02-16 23:26:16 +0000

    feat: add homepage template

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+    </Routes>
+  );
+}
+
+export default App;
