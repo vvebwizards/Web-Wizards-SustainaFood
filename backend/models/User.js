176e857a5607388a1f9997a302746ccc64f0616d
soufienaessalah <soufien.ahmedessalah@esprit.tn>
2025-02-16 23:51:46 +0000

    feat: add User model with schema and basic validation

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { 
+    type: String, 
+    enum: ["donor", "recipient", "volunteer"], 
+    required: true
+  },
+  blocked: { 
+    type: Boolean, 
+    default: false,
+  },
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
